23b5d59c2090b9f3a0d5ee2fb2afabb54484ec33
EDDAHABI Riad <eddahabi.riad@gmail.com>
2021-09-30 18:50:23 +0200

    🗃️ Pagination for the /iso_code endpoint

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.params import Query
+from fastapi_pagination import Page, Params, paginate
+from typing import TypeVar, Generic
+from fastapi_pagination.default import Page as BasePage, Params as BaseParams
 from pydantic import BaseModel
 from typing import (Dict, List, Optional, Set, Tuple)
 import json
+
+T = TypeVar("T")
+
+class Params(BaseParams):
+    size: int = Query(5, ge=1, le=1_000, description="Page size")
+
+class Page(BasePage[T], Generic[T]):
+    __params_type__ = Params
 
 class Country(BaseModel):
     id: str
@@ -29,8 +40,8 @@ def home():
     print({"id": data["features"][0]["id"], "name": data["features"][0]["properties"]["name"], "geometry_type": data["features"][0]["geometry"]["type"], "geometry": list(data["features"][0]["geometry"]["coordinates"])})
     return {"id": data["features"][0]["id"], "name": data["features"][0]["properties"]["name"], "geometry_type": data["features"][0]["geometry"]["type"], "geometry": list(data["features"][0]["geometry"]["coordinates"])}
 
-@app.get("/iso_code", response_model=List[isoName])
-def iso_code(*, names: List = Query(...), details: Optional[bool] = None): #Ellipsis (...) enables to make a parameter required
+@app.get("/iso_code", response_model=Page[isoName])
+def iso_code(*, names: List = Query(...), params: Params = Depends(), details: Optional[bool] = None): #Ellipsis (...) enables to make a parameter required
     isoNameReturnList = []
     for entry in data["features"]:
         if entry["properties"]["name"] in names:
@@ -38,7 +49,7 @@ def iso_code(*, names: List = Query(...), details: Optional[bool] = None): #Elli
                 isoNameReturnList.append({"id": entry["id"], "name": entry["properties"]["name"], "geometry_type": entry["geometry"]["type"], "coordinates": entry["geometry"]["coordinates"]})
             else:
                 isoNameReturnList.append({"id": entry["id"], "name": entry["properties"]["name"]})
-    return isoNameReturnList
+    return paginate(isoNameReturnList, params)
 
 
 @app.get("/all_geometries")
